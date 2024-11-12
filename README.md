14474753d5129c494e98a01915ab93989b41392b
unknown <hasan.cse.200104009@aust.edu>
2024-11-12 21:42:49 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## How to run the code
+- git clone 
+- cd laravel-starter
+- cp .env.example `.env`
+- open .env and update DB_DATABASE (database details)
+- `composer install`
+- `php artisan key:generate`
+- `php artisan migrate:fresh --seed`
+- `php artisan serve`
+
+
+## Credentials
+- #### Admin
+- email: kamrul@gmail.com
+- password : password
+- #### Writer
+- email: hasan@gmail.com
+- password: password
---
+++ resources/lang/en/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Password has been reset!',
+    'sent' => 'We have emailed your password reset link!',
+    'throttled' => 'Please wait.',
+    'token' => 'Token is invalid.',
+    'user' => "Email not matched.",
+
+];
